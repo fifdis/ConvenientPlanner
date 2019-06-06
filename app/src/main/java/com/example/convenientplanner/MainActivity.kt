542e5c4b7d695f77430fa0ad26b49c5047e46de1
fifdis <51359570+fifdis@users.noreply.github.com>
2019-06-06 11:06:00 +0300

    msg

--- app/src/main/java/com/example/convenientplanner/MainActivity.kt
+++ app/src/main/java/com/example/convenientplanner/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.convenientplanner
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        settings.setOnClickListener {
+            val intent = Intent(this, activity_options::class.java)
+            startActivity(intent)
+        }
+        list.setOnClickListener {
+            val intent = Intent(this, activity_options::class.java)
+            startActivity(intent)
+        }
+        newtsk.setOnClickListener {
+            val intent = Intent(this, activity_options::class.java)
+            startActivity(intent)
+        }
+        calend.setOnClickListener {
+            val intent = Intent(this, activity_options::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
